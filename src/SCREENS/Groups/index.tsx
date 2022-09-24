d9576848dfffd7fe997d712d3b0e63e7395972fe
feliippeee <felipe.tecnosuporte@gmail.com>
2022-09-24 01:00:50 -0300

    Estrutura do projeto - Path Mapping

---
+++ src/SCREENS/Groups/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { styles } from './styles';
+
+export function Groups() {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+}
