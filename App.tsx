3acf217b156438e5082d5104b352f57c0d10f243
feliippeee <felipe.tecnosuporte@gmail.com>
2022-09-25 01:19:25 -0300

    CSS-in-JS - Utilizando tema

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { Groups } from "@screens/Groups";
-import theme from "src/theme";
+import theme from "./src/theme";
 import { ThemeProvider } from "styled-components";
 
 export default function App() {
