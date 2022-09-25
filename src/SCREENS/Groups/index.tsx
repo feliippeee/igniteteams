62bfce8a8f3afd06c841cda8d078ba4bba6b3a72
feliippeee <felipe.tecnosuporte@gmail.com>
2022-09-25 10:59:20 -0300

    CSS-in-JS - Criando Componente GroupCard

--- src/SCREENS/Groups/index.tsx
+++ src/SCREENS/Groups/index.tsx
@@ -1,10 +1,21 @@
 import { Header } from '@components/Header';
+import { GroupCard } from '@components/GroupCard';
+
+import { Highlight } from '@components/Highlight';
 import { Container } from './styles';
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight 
+        title="Turmas"
+        subtitle="Jogue com sua turma"
+      />
+      <GroupCard 
+        title="Galera do Ignite"
+        
+      />
     </Container>
   );
 }
