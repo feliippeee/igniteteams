6de31c7b70531bcf4dd2a1768a0fc120671ec971
feliippeee <felipe.tecnosuporte@gmail.com>
2022-09-26 18:10:17 -0300

    CSS-in-JS - Criando Componente Input

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components/native";
 
 export type ButtonTypeStyleProps = 'PRIMARY' | 'SECONDARY';
 
-type Props = {
+type Props = TouchableOpacity &{
     type: ButtonTypeStyleProps;
 }
 
