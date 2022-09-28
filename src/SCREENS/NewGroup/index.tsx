901dad3a809e6eeae3caa1697f7a9cd6457579e1
feliippeee <felipe.tecnosuporte@gmail.com>
2022-09-28 00:47:49 -0300

    Navegação - Navegando pela aplicação

--- src/SCREENS/NewGroup/index.tsx
+++ src/SCREENS/NewGroup/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import { Container, Content, Icon } from './styles';
 import { Header } from '@components/Header';
 import { Button } from '@components/Button';
@@ -5,6 +6,12 @@ import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
 
 export function NewGroup(){
+    const navigation = useNavigation();
+
+    function handleNew() {
+        navigation.navigate('players', {group: 'Rocket'})
+    }
+
     return (
         <Container>
             <Header showBackButton />
@@ -24,6 +31,7 @@ export function NewGroup(){
                 <Button 
                     title="Criar"
                     style={{ marginTop: 20 }}
+                    onPress={handleNew}
                 />
             </Content>
 
