af54bbc587e1c2ded08fed6747cc39f71edb9415
feliippeee <felipe.tecnosuporte@gmail.com>
2022-09-28 23:51:29 -0300

    Navegação - Passando parâmetros pela rota

--- src/SCREENS/NewGroup/index.tsx
+++ src/SCREENS/NewGroup/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { Container, Content, Icon } from './styles';
 import { Header } from '@components/Header';
@@ -6,10 +7,12 @@ import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
 
 export function NewGroup(){
+    const [group, setGroup] = useState('');
+    
     const navigation = useNavigation();
 
     function handleNew() {
-        navigation.navigate('players', {group: 'Rocket'})
+        navigation.navigate('players', { group })
     }
 
     return (
@@ -26,6 +29,7 @@ export function NewGroup(){
 
                 <Input 
                     placeholder='Nome da turma'
+                    onChangeText={setGroup}
                 />
 
                 <Button 
