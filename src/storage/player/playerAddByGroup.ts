16de91fa14f428d2561cc7ce54d49710d8a8e525
feliippeee <felipe.tecnosuporte@gmail.com>
2022-09-30 12:33:12 -0300

    Local Storage - Listando Players

--- src/storage/player/playerAddByGroup.ts
+++ src/storage/player/playerAddByGroup.ts
@@ -4,11 +4,21 @@ import { AppError } from '@utils/AppError';
 import { PLAYER_COLLECTION } from '@storage/storageConfig';
 
 import { PlayerStorageDTO } from './playerStorageDTO';
+import { playerGetByGroup } from "./playersGetByGroup";
 
 export async function playerAddByGroup(newPlayer: PlayerStorageDTO, group: string) {
     try {
+        const storedPlayers = await playerGetByGroup(group) 
+        
+        const playerAlreadyExists = storedPlayers.filter(player => player.name === newPlayer.name);
 
-        await AsyncStorage.setItem(`${PLAYER_COLLECTION}-${group}`, '');
+        if(playerAlreadyExists.length > 0) {
+            throw new AppError('Essa pessoa já está adicionada em um time aqui.')
+        }
+
+        const storage = JSON.stringify({...storedPlayers, newPlayer});
+
+        await AsyncStorage.setItem(`${PLAYER_COLLECTION}-${group}`, storage);
     } catch (error) {
         throw (error);
     }
