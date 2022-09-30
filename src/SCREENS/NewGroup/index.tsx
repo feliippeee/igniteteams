f5d8436ca8ce93391beeb2c07dbf8c7541ff022d
feliippeee <felipe.tecnosuporte@gmail.com>
2022-09-30 00:30:14 -0300

    Local Storage - Tratando exceções

--- src/SCREENS/NewGroup/index.tsx
+++ src/SCREENS/NewGroup/index.tsx
@@ -1,7 +1,11 @@
 import { useState } from 'react';
 import { groupCreate } from '@storage/group/groupCreate';
+import { Alert } from 'react-native';
+import { AppError } from '@utils/AppError';
 import { useNavigation } from '@react-navigation/native';
+
 import { Container, Content, Icon } from './styles';
+
 import { Header } from '@components/Header';
 import { Button } from '@components/Button';
 import { Highlight } from '@components/Highlight';
@@ -14,12 +18,21 @@ export function NewGroup(){
     
     async function handleNew() {
         try { 
+            if(group.trim().length === 0){
+                return Alert.alert('Novo Grupo', 'Informe o nome da turma.');
+            }
             await groupCreate(group);
             navigation.navigate('players', { group })
 
         } catch (error) {
-            console.log(error);
+            if (error instanceof AppError) {
+                Alert.alert('Novo Grupo', error.message);
+            }else{
+                Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo');
+                console.log(error);
+            }
         }
+        
     }
 
     return (
